445a32d663b963a650615eba083c7d953ae6ef76
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-08-27 23:15:36 +0530

    2D arrays: Practice Qs 2 done.

--- DSA/2Darrays/PracticeQs.cpp
+++ DSA/2Darrays/PracticeQs.cpp
@@ -2,26 +2,50 @@
 #include <utility>
 using namespace std;
 
-int allSevens(int matrix[][3], int n, int m){
-    int sevens = 0;
-    for(int i=0; i <n ; i++){
-        for(int j=0; j < m; j++){
-            if(matrix[i][j] == 7){
-                sevens += 1;
-            }
-        }
+// Practice Qs: 1
+// int allSevens(int matrix[][3], int n, int m){
+//     int sevens = 0;
+//     for(int i=0; i <n ; i++){
+//         for(int j=0; j < m; j++){
+//             if(matrix[i][j] == 7){
+//                 sevens += 1;
+//             }
+//         }
+//     }
+//     return sevens;
+// }
+
+// int main(){
+//     int matrix[2][3] = {
+//         {4,7,8},
+//         {8,8,7},
+//     };
+//     int n = 2, m = 3;
+
+//     cout<<allSevens(matrix, n, m)<<"\n";
+
+//     return 0;
+// }
+
+
+// Practice Qs: 2
+int sum(int nums[][3], int n, int m, int row){
+    int sum=0, rowIdx = row - 1;
+    for(int j=0; j<n; j++){
+        sum += nums[rowIdx][j];
     }
-    return sevens;
+    return sum;
 }
 
 int main(){
-    int matrix[2][3] = {
-        {4,7,8},
-        {8,8,7},
+    int nums[3][3] = {
+        {1,4,9},
+        {11,4,3},
+        {2,2,3},
     };
-    int n = 2, m = 3;
+    int n=3, m=3;
 
-    cout<<allSevens(matrix, n, m)<<"\n";
+    cout<<sum(nums, n, m, 1)<<"\n";
 
     return 0;
 }
