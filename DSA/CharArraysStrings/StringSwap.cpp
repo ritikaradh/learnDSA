8ddc3f2125121239afa707de0ba8842bd76725ad
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-08-30 20:49:22 +0530

    string swap with optimized version of brute force.

--- DSA/CharArraysStrings/StringSwap.cpp
+++ DSA/CharArraysStrings/StringSwap.cpp
@@ -7,15 +7,35 @@ bool stringSwap(string str1, string str2){
         return false;
     }
 
+    int size = str1.length();
     
+    for(int i = 0; i < size; i++){
+        for(int j=i+1; j < size; j++){
+            string temp = str2;
+            swap(temp[i], temp[j]);
+            if(temp == str1){
+                return true;
+            }
+        }
+    }
+
+    for(int i = 0; i < size; i++){
+        for(int j=0; j < size; j++){
+            string temp = str1;
+            swap(temp[i], temp[j]);
+            if(temp == str2){
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
 
 int main(){
-    string str1, str2;
-    getline(cin, str1);
-    getline(cin, str2);
+    string str1 = "kanb", str2 = "bank";
 
-    stringSwap(str1, str2);
+    cout<<stringSwap(str1, str2)<<"\n";
 
     return 0;
 }
