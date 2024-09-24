f0bab4732013429e26431bf92d08b7ed1681d418
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-09-24 14:22:39 +0530

    Recursion Problem Solving: Remove duplicates in String

---
+++ DSA/RecursionAdv/RemoveDuplicates.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string rmDuplicate(string str, bool* map, string ans, int i) {
+    if(i == str.length()) return ans;
+    int idx = str[i] - 'a';
+    if(map[idx] == false) {
+        ans += str[i];
+        map[idx] = true;
+    }
+    return rmDuplicate(str, map, ans, i+1);
+}
+
+int main(){
+    bool map[26] = {false};
+    cout<<rmDuplicate("apnacollege", map, "", 0)<<endl;
+
+    return 0;
+}
