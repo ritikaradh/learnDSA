26002e8e01865f47111e3630ead1e66130decb20
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-09-27 16:05:59 +0530

    Resolved corner case issues of Search in Rotated Sorted Array

--- DSA/DivideAndConquer/RotatedSortedArray.cpp
+++ DSA/DivideAndConquer/RotatedSortedArray.cpp
@@ -14,12 +14,12 @@ int rotatedSortedArr(vector<int> &nums, int target, int start, int end){
 
     // conquer
     if(nums[start] <= nums[mid]){ // L1
-        if(nums[start] <= target && nums[mid-1] >= target) // case1
+        if(nums[start] <= target && nums[mid] > target) // case1
             return rotatedSortedArr(nums, target, start, mid-1);   // left subarray
         else    // case2
             return rotatedSortedArr(nums, target, mid+1, end); // right subarray
     }else{  // L2
-        if(nums[mid+1] <= target && nums[end] >= target)    // case3
+        if(nums[mid] < target && nums[end] >= target)    // case3
             return rotatedSortedArr(nums, target, mid+1, end);  // right subarray
         else    // case 4
             return rotatedSortedArr(nums, target, start, mid-1);
@@ -27,8 +27,10 @@ int rotatedSortedArr(vector<int> &nums, int target, int start, int end){
 }
 
 int main(){ 
-    vector<int> nums = {7, 8, 9, 0, 2, 3, 4, 6};
-    cout<<rotatedSortedArr(nums, 2, 0, nums.size()-1)<<endl;
-    cout<<rotatedSortedArr(nums, 3, 0, nums.size()-1)<<endl;
+    // vector<int> nums = {7, 8, 9, 0, 2, 3, 4, 6};
+    // cout<<rotatedSortedArr(nums, 2, 0, nums.size()-1)<<endl;
+    // cout<<rotatedSortedArr(nums, 3, 0, nums.size()-1)<<endl;
+    vector<int> nums = {7};
+    cout<<rotatedSortedArr(nums, 9, 0, nums.size()-1)<<endl;
     return 0;
 }
