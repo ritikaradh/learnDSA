e7096c4de33d88fb656e0a6b700d01bee5e6b947
Ritika Upadhyay <thecatiscoding@outlook.com>
2024-12-04 23:49:01 +0530

    Rehashing while insertion in Hash-table and worst case time complexity of insertion fucntion in hash-table.

--- DSA/Hashing/Implementation.cpp
+++ DSA/Hashing/Implementation.cpp
@@ -16,6 +16,11 @@ class Node{
         this->val = val;
         this->next = NULL;
     }
+
+    //destructor
+    ~Node(){
+        if(next) delete next;
+    }
 };
 
 class HashTable{
@@ -35,6 +40,35 @@ class HashTable{
         return idx;
     }
 
+    //rehash-function
+    void rehash() { //O(n)
+        Node** oldTable = table;
+        int oldSize = totSize;
+
+        totSize = 2*totSize;
+
+        table = new Node*[totSize];
+
+        //initialize new table with null value
+        for(int i=0; i<totSize; i++){
+            table[i] = NULL;
+        }
+
+        //copy old table to new table
+        for(int i = 0; i < oldSize; i++) {
+            Node* temp = oldTable[i];
+
+            while(temp) {
+                insert(temp->key, temp->val);
+                temp = temp->next;
+            }
+
+            if(oldTable[i]) delete oldTable[i];
+        }
+
+        delete[] oldTable;
+    }
+
     public:
     
     //constructor
@@ -52,7 +86,7 @@ class HashTable{
     }
     
     //insertion in hash-table
-    void insert(string key, int val) {
+    void insert(string key, int val) {  //O(1) avg
         int idx = hashFunction(key);
 
         Node* newNode = new Node(key, val);
@@ -62,6 +96,10 @@ class HashTable{
         head = newNode;
 
         currSize++;
+
+        double lambda = currSize / (double)totSize;
+
+        if(lambda > 1) rehash(); //O(n) - worst t.c
     }
 
     //deletion in hash-table
