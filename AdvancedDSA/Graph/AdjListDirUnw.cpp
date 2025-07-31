06f3e7eb27e2fd4a9f6877c440d5e7fc0529f2a7
Ritika Upadhyay <thecatiscoding@outlook.com>
2025-07-31 05:43:06 +0530

    Graph Implementation - Directed, Unweighted - using Adjacency Matrix

---
+++ AdvancedDSA/Graph/AdjListDirUnw.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Graph{
+    //data members
+    int sizeOfGraph;
+    list<int>* vectorsList;
+
+    public:
+
+    //constructor
+    Graph(int sizeOfGraph){
+        this->sizeOfGraph = sizeOfGraph;
+        vectorsList = new list<int>[sizeOfGraph];
+    }
+
+    //associate edges with vectors
+    void addEdge(int vector, int neighbor){
+        vectorsList[vector].push_back(neighbor);
+        cout<<vector<<" ---> "<<neighbor<<endl;
+    }
+
+    //print graph
+    void printGraph(){
+        for(int i=0; i<sizeOfGraph; i++){
+            list<int> neighbors = vectorsList[i];
+            cout<<i<<" : ";
+
+            for(int neighbor : neighbors){
+                cout<<neighbor<<" , ";
+            }
+
+            cout<<endl;
+        }
+    }
+};
+
+int main(){
+    Graph graph(5);
+    
+    graph.addEdge(0,1);
+    graph.addEdge(1,2);
+    graph.addEdge(2,3);
+    graph.addEdge(2,4);
+    graph.addEdge(3,1);
+    
+    graph.printGraph();
+    
+    return 0;
+}
